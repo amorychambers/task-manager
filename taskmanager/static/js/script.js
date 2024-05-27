bc4ee306945155702e7cbc7eeb643b2f723cadfc
Amory <108595086+amorychambers@users.noreply.github.com>
2024-05-27 18:43:18 +0100

    expand add_task form

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     let modals = document.querySelectorAll('.modal')
+    let datepicker = document.querySelectorAll('.datepicker')
     M.Sidenav.init(sidenav);
     M.Modal.init(modals)
+    M.Datepicker.init(datepicker, {format: "dd mmmm, yyyy", i18n: {done: "Select"}})
   });
