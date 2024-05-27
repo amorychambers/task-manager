e80de56e82c1d147301b50dde58d10c233523f8d
Amory <108595086+amorychambers@users.noreply.github.com>
2024-05-27 14:06:18 +0100

    add functionality to delete categories with confirmation modal

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
+    let modals = document.querySelectorAll('.modal')
     M.Sidenav.init(sidenav);
+    M.Modal.init(modals)
   });
