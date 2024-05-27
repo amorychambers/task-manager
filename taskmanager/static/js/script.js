ad5c58f68e328494a21b88fdcc6efc9f36335b3f
Amory <108595086+amorychambers@users.noreply.github.com>
2024-05-27 19:30:08 +0100

    complete add_task functionality with for loop for categories in database

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     let modals = document.querySelectorAll('.modal')
     let datepicker = document.querySelectorAll('.datepicker')
+    let selector = document.querySelectorAll('select')
     M.Sidenav.init(sidenav);
     M.Modal.init(modals)
     M.Datepicker.init(datepicker, {format: "dd mmmm, yyyy", i18n: {done: "Select"}})
+    M.FormSelect.init(selector)
   });
