5ec3c02fd18b73ea62d49c206aa77f74986680bb
Amory <108595086+amorychambers@users.noreply.github.com>
2024-05-28 22:30:33 +0100

    add edit_task.html and functionality to update tasks in database

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -37,7 +37,6 @@ def delete_category(category_id):
     db.session.commit()
     return redirect(url_for("categories"))
 
-
 @app.route("/add_task", methods=["GET", "POST"])
 def add_task():
     categories = list(Category.query.order_by(Category.category_name).all())
@@ -60,3 +59,17 @@ def delete_task(task_id):
     db.session.delete(task)
     db.session.commit()
     return redirect(url_for("home"))
+
+@app.route("/edit_task/<int:task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    categories = list(Category.query.order_by(Category.category_name).all())
+    task = Task.query.get_or_404(task_id)
+    if request.method == "POST":
+        task.task_name = request.form.get("task_name")
+        task.task_description = request.form.get("task_description")
+        task.due_date = request.form.get("due_date")
+        task.is_urgent = bool(True if request.form.get("is_urgent") else False)
+        task.category_id = request.form.get("category_id")
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("edit_task.html", task=task, categories=categories)
